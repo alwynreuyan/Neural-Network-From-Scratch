d601f28df4fc231a11fe8efa7265f4f2f4f6112b
alwynreuyan <alwynjadelreuyan@gmail.com>
2026-05-21 01:12:00 +0900

    first sample neuron

---
+++ lp.py
@@ -0,0 +1,7 @@
+input = [2, 3, 4, 5, 6]
+weight = [1, 3, 4, 5, 3]
+bias = 4
+
+output = [input[0]*weight[0] + input[1]*weight[1] + input[2]*weight[2] + input[3]*weight[3] + input[4]*weight[4] + bias] 
+
+print(output)
